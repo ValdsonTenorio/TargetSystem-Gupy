b5bf9fa8010d0d30220d4e1ef8764439416e0ffe
Valdson Martins Tenório <67640614+ValdsonTenorio@users.noreply.github.com>
2024-03-14 16:21:15 -0400

    Add files via upload

---
+++ TargetSistem/src/Main.java
@@ -0,0 +1,13 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        int i = 13; int s = 0; int k = 0;
+        while (k < i){
+            k = k +1;
+            s = s + k;
+        }
+        System.out.println(s);
+    }
+}
